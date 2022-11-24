68f03a5a618040f11cfeb392a482fc6c00614087
Ena Salaj <86615856+esalaj4@users.noreply.github.com>
2022-11-24 10:25:48 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,20 +1,26 @@
 <?php
 
 class Connection
-{
+{   public $dbh;
     private $host = "localhost";
-    private $database = "enas";
+    private $database = "praksa";
     private $username = "ena";
     private $password = "password";
 
-    public function connect()
-    { 
-    try {
-        $conn = new PDO("mysql:host=$this->host;dbname=$this->database",$this->username,$this->password);
-        echo "connected";
-    } catch(PDOException $pe) {
-        die("Couldn't connect to db" . $pe->getMessage());
+
+    public function __construct() {
+        
+        $options = array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        
+        try {
+            $this->dbh =  new PDO("mysql:host=$this->host;dbname=$this->database",$this->username,$this->password);
+            echo "connected";
+        }
+        catch (PDOException $pe) {
+            die("Couldn't connect to db" . $pe->getMessage());
+        }
     }
 }
-}
+
+$connection = new connection();
 ?>
