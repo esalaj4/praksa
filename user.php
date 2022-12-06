f32af19eaeef52d51185d7d665c24b233a8b7e88
Ena Salaj <86615856+esalaj4@users.noreply.github.com>
2022-12-06 14:12:14 +0100

    Update user.php

--- user.php
+++ user.php
@@ -1,22 +1,38 @@
 <?php 
 include('model.php');
+
 class User extends Model{
 
-    
     private $connection;
-    public function __construct($connection)
+    public function __construct()
     {
-        parent::__construct($connection);
         $this->table='users';
         $this->allowed=['name','surname'];
-        
     } 
 }
 
-$user2=new user($connection);
+$user1 = new user();
+$user2=new user();
+$user3 = new user();
+$user4=new user();
+$user5=new user();
+
+$user1->name="toni";
+$user1->surname="saljj";
 $user2->name="ena";
 $user2->surname="salaj";
+$user3->name="name3";
+$user3->surname="surname3";
+$user4->name="name4";
+$user4->surname="surname4";
+$user5->name="name5";
+$user5->surname="surname5";
 
-$user2->filterByProperty('surname');
+$user1->save();
+/* echo '<pre>';
+print_r(User::all());
+echo '<pre>';  
+ */
+//$user3->delete();
 
-?>
+
